996f126025291ce045a6767578f8420683f2c93c
alejandrogarciasalas <garciasalasalejandro@gmail.com>
2016-02-13 18:40:22 -0800

    working prototype

---
+++ js/main.js
@@ -0,0 +1,28 @@
+var start = new Date;
+
+$("#u_0_1").append("<div class='_1uh-'><p id='timer'></p></div>");
+
+var store_timer_seconds = function() {
+	        var secondsSpentOnFacebook = $("#timer").data('seconds');
+	        chrome.storage.sync.set({'timeSpentOnFacebook': secondsSpentOnFacebook});
+}
+
+var timer_settings = {
+	format: "%H:%M:%S",
+	duration: "1s",
+    callback: store_timer_seconds,
+    repeat: true
+}
+
+chrome.storage.sync.get('timeSpentOnFacebook', function(result){
+	var timeSpentOnFacebook = result["timeSpentOnFacebook"];
+
+	if(timeSpentOnFacebook){
+		timer_settings["seconds"] = timeSpentOnFacebook;
+		$("#timer").timer(timer_settings);
+	} 
+	else {
+		$("#timer").timer(timer_settings);
+	}
+});
+
